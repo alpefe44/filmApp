5524974608a7337d43cfef02cefd5da1e67e1d7c
Alp <efe1998alp@hotmail.com>
2023-09-09 16:15:38 +0300

    .

--- components/movieList.js
+++ components/movieList.js
@@ -6,13 +6,13 @@ import { useNavigation } from '@react-navigation/native';
 const height = Dimensions.get('screen').height;
 const width = Dimensions.get('screen').width;
 
-const MovieList = ({ data, title }) => {
+const MovieList = ({ data, title , hideSeeAll = true }) => {
 
   return (
     <View>
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
         <Text style={{ paddingHorizontal: 16, fontSize: 16, fontWeight: 'bold', color: 'white' }}>{title}</Text>
-        <Text style={{ paddingHorizontal: 16, fontSize: 16, fontWeight: 'bold', color: '#ff7f00' }}>See All</Text>
+        <Text style={{ paddingHorizontal: 16, fontSize: 16, fontWeight: 'bold', color: '#ff7f00' }}>{hideSeeAll ? "See All" : ''}</Text>
       </View>
 
       <FlatList
--- screens/Person.js
+++ screens/Person.js
@@ -2,10 +2,12 @@ import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Ionicons } from '@expo/vector-icons';
+import MovieList from '../components/movieList';
 
 const Person = () => {
 
   const [isPress, setIsPress] = useState(false);
+  const [personMovie, setPersonMovie] = useState([1, 2, 3, 4]);
   return (
     <ScrollView style={{ backgroundColor: '#1f201c' }}>
       <View>
@@ -43,22 +45,37 @@ const Person = () => {
 
         <View style={{ height: 60, backgroundColor: '#61677A', marginTop: 15, flexDirection: 'row', alignItems: 'center', marginHorizontal: 30, justifyContent: 'center', borderRadius: 25 }}>
           <View style={{ alignItems: 'center', justifyContent: 'center', borderRightWidth: 2, paddingHorizontal: 5, borderColor: 'white' }}>
-            <Text style={{ fontSize: 12, fontWeight: '500', color: 'white' }}>Gender</Text>
+            <Text style={{ fontSize: 16, fontWeight: '500', color: 'white' }}>Gender</Text>
             <Text style={{ fontSize: 10, fontWeight: '400', color: 'white' }}>asfsaf</Text>
           </View>
           <View style={{ alignItems: 'center', justifyContent: 'center', borderRightWidth: 2, paddingHorizontal: 5, borderColor: 'white' }}>
-            <Text style={{ fontSize: 12, fontWeight: '500', color: 'white' }}>Birthday</Text>
+            <Text style={{ fontSize: 16, fontWeight: '500', color: 'white' }}>Birthday</Text>
             <Text style={{ fontSize: 10, fontWeight: '400', color: 'white' }}>asfsaasfasff</Text>
           </View>
           <View style={{ alignItems: 'center', justifyContent: 'center', borderRightWidth: 2, paddingHorizontal: 5, borderColor: 'white' }}>
-            <Text style={{ fontSize: 12, fontWeight: '500', color: 'white' }}>Known For</Text>
+            <Text style={{ fontSize: 16, fontWeight: '500', color: 'white' }}>Known For</Text>
             <Text style={{ fontSize: 10, fontWeight: '400', color: 'white' }}>asfsaf</Text>
           </View>
           <View style={{ alignItems: 'center', justifyContent: 'center', paddingHorizontal: 5, borderColor: 'white' }}>
-            <Text style={{ fontSize: 12, fontWeight: '500', color: 'white' }}>Popularity</Text>
+            <Text style={{ fontSize: 16, fontWeight: '500', color: 'white' }}>Popularity</Text>
             <Text style={{ fontSize: 10, fontWeight: '400', color: 'white' }}>asfsaf</Text>
           </View>
         </View>
+      </View>
+
+      <View style={{ marginTop: 10, marginHorizontal: 10 }}>
+        <View>
+          <Text style={{ color: 'white', fontSize: 16, fontWeight: '800' }}>Biography</Text>
+          <Text style={{ fontSize: 13, color: 'white' }}>1964 Lübnan doğumlu Kanada’lı ünlü oyuncu ve müzisyen Keanu Reeves oyunculuk kariyerinin yanında kendi grubu The Dogstar’da bas gitar çalmaktadır.  Anne ve babası boşandıktan sonra annesi kız kardeşiyle Keanu’yu alarak New York’a taşınmıştır. Dört lise değiştiren ve hiç birinden mezun olamayan Keanu 15 yaşında Wolfboy adlı oyunla Toronto’da oyunculuk kariyerine başladı. Hokey takımına giren ve The Wall takma adını alan ünlü oyuncu sakatlanıp sporu bırakmak zorunda kalır.
+
+            1993’te en iyi arkadaşı River Phoenix’i kaybeder. Kız kardeşinin üzücü bir şekilde lösemi hastası olması , erkek kardeşinin ise devamlı girip çıktığı hapis maceraları yüzünden kendi hayatını bir türlü yaşayamaz. Reeves birkaç sene sonra Jennifer Syme ile tanışır , 1999’da  sevgilisi düşük yapar. Birkaç yıl sonra, Reeves’in “absürt” olarak tanımladığı trajik olay hayatının son çivisini de çakar; Jennifer Syme, katıldığı bir parti çıkışında  trafik kazası geçirerek  hayatını kaybeder.
+
+            Al Pacino, Laurence Fishburne, Hugo Weaving, Charlize Theron gibi sinema tarihinde yer etmiş bir çok ünlü oyuncuyla birlikte rol alan Reeves’in filmlerinden bazıları; Şeytanın Avukatı, John Wick, 47 Ronin, Constantine, Speed ve Kasımda Aşk Başka'dır. En çok ses getiren filmi The Matrix’tir. Matrix filminden kazandığı 75 milyon doları set ekibine bağışlamıştır. Keanu Reeves’in Hollywood Bulvarı’nda yıldızı bulunmaktadır.</Text>
+        </View>
+      </View>
+
+      <View style={{ marginTop: 25 }}>
+        <MovieList data={personMovie} title={'Films'} hideSeeAll = {false}></MovieList>
       </View>
 
 
